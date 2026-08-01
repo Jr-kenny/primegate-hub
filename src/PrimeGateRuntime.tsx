e6943524551146bfdf4390c6deb08aa925e0271a
jr-kenny <dalvidjr2022@gmail.com>
2026-08-01 11:57:32 +0100

    fix: isolate Shelby from public page boot

--- src/PrimeGateRuntime.tsx
+++ src/PrimeGateRuntime.tsx
@@ -1,9 +1,7 @@
 import { MotionConfig } from "framer-motion";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ShelbyClientProvider } from "@shelby-protocol/react";
 
 import App from "./App";
-import { shelbyClient } from "./config/web3";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
 import { Web3Providers } from "./components/Web3Providers";
 import { PrimeGateWalletProvider } from "./hooks/usePrimeGateWallet";
@@ -25,13 +23,11 @@ export default function PrimeGateRuntime() {
     <ThemeProvider>
       <MotionConfig reducedMotion="user">
         <QueryClientProvider client={queryClient}>
-          <ShelbyClientProvider client={shelbyClient}>
-            <Web3Providers>
-              <PrimeGateWalletProvider>
-                <App />
-              </PrimeGateWalletProvider>
-            </Web3Providers>
-          </ShelbyClientProvider>
+          <Web3Providers>
+            <PrimeGateWalletProvider>
+              <App />
+            </PrimeGateWalletProvider>
+          </Web3Providers>
         </QueryClientProvider>
       </MotionConfig>
     </ThemeProvider>
